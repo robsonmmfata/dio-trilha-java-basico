4e11580789cb9ad354dca7f31f137c4d4f4cf741
Robson Alex <carloscostammfata@gmail.com>
2024-08-05 21:25:41 -0300

    aula sobre condicional composta java spring boot DIO

--- tipos-de-variaveis/src/CaixaEletronico.java
+++ tipos-de-variaveis/src/CaixaEletronico.java
@@ -3,10 +3,13 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 15.0;
 
         if (valorSolicitado < saldo)
         saldo = saldo - valorSolicitado ;
+       else
+       System.out.println("Saldo insuficiente");
+
         System.out.println(saldo);
     }
 }
