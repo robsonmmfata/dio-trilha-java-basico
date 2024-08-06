8a656d95a37c98ea8653ca3aa046f82c11d187ab
Robson Alex <carloscostammfata@gmail.com>
2024-08-05 21:09:24 -0300

    aula bootcamp dio javaspringboot(condicional simples)

---
+++ tipos-de-variaveis/src/CaixaEletronico.java
@@ -0,0 +1,12 @@
+
+//aula sobre condicional simples(controle de fluxo simples)
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        double saldo = 25.0;
+        double valorSolicitado = 17.0;
+
+        if (valorSolicitado < saldo)
+        saldo = saldo - valorSolicitado ;
+        System.out.println(saldo);
+    }
+}
