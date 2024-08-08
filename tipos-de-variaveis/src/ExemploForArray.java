aa034bf45d8a5819f950cc47c379af5f97958861
Robson Alex <carloscostammfata@gmail.com>
2024-08-07 22:23:36 -0300

    finalizado aula sobre for em arrays

---
+++ tipos-de-variaveis/src/ExemploForArray.java
@@ -0,0 +1,11 @@
+public class ExemploForArray {
+
+    public static void main(String[] args) {
+//em arrays o indice se inicia em 0
+
+        String alunos[] = { "FELIPE" , "JONAS" , "JULIA" , "MARCOS" };
+        for (int X = 0;  X  <  alunos.length;  X++) {
+            System.out.println("O aluno no indice x= " +X + " é " + alunos [X]);
+    }
+}
+}
---
+++ tipos-de-variaveis/src/ExemploForreach.java
@@ -0,0 +1,11 @@
+public class ExemploForreach {
+    public static void main(String[] args) {
+        String aluno[] = { "FELIPE" , "JONAS" , "JULIA" , "MARCOS" };
+
+        
+        for(String aluno : aluno){
+            System.out.println("Nome do Aluno é:" + aluno);
+
+        }
+}   
+}
