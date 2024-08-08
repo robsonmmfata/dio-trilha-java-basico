6edb2602bfa081172cd1dbb359696989aa621a8c
Robson Alex <carloscostammfata@gmail.com>
2024-08-07 22:35:33 -0300

    mais uma aula terminada sobre break e continue

---
+++ tipos-de-variaveis/src/ExemploBreakContinue.java
@@ -0,0 +1,9 @@
+public class ExemploBreakContinue {
+    public static void main(String[] args) {
+        for(int numero =1; numero <= 5; numero ++){
+            if(numero == 3) continue;
+//qndo for usar o break é so subsittuir pelo continue,break ele para a aplicação em certa instancia
+            System.out.println(numero);
+}
+}
+}
